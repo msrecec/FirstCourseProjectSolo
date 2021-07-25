e59f5cd29652a549d49babb2118e86bd63359a26
msrecec <67606889+msrecec@users.noreply.github.com>
2021-07-25 20:48:34 +0200

    added some buttons

--- src/app/recipe-form/recipe-form.component.ts
+++ src/app/recipe-form/recipe-form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../models/ingredient.model';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-form',
   templateUrl: './recipe-form.component.html',
-  styleUrls: ['./recipe-form.component.css']
+  styleUrls: ['./recipe-form.component.css'],
 })
 export class RecipeFormComponent implements OnInit {
+  @Input() ingredients?: Ingredient[];
+  @Output() addRecipe = new EventEmitter<Recipe>();
+  selectedIngredients: Ingredient[] = [];
 
-  constructor() { }
+  name?: string;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  toggleIngredient(ingredient: Ingredient) {
+    ingredient.selected = !ingredient.selected;
   }
 
+  ngOnInit(): void {}
 }
