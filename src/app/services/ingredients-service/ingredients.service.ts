274d904f026f32868be4cce19035543ce72bd899
msrecec <67606889+msrecec@users.noreply.github.com>
2021-08-05 21:40:07 +0200

    implemented addition methods

--- src/app/services/ingredients-service/ingredients.service.ts
+++ src/app/services/ingredients-service/ingredients.service.ts
@@ -19,5 +19,37 @@ export class IngredientsService {
     return this.ingredients;
   }
 
+  /**
+   * Compares the two ingredients
+   *
+   * @param ingredient1
+   * @param ingredient2
+   * @returns true if the ingredients are equal
+   */
+
+  private compareIngredients(
+    ingredient1: Ingredient,
+    ingredient2: Ingredient
+  ): boolean {
+    return (
+      ingredient1.name.localeCompare(ingredient2.name) === 0 &&
+      ingredient1.halal === ingredient2.halal
+    );
+  }
+
+  public addIngredient(ingredient: Ingredient): void {
+    let cmp = false;
+    this.ingredients.forEach((i) => {
+      if (this.compareIngredients(i, ingredient)) {
+        cmp = true;
+      }
+    });
+    if (cmp) {
+      alert('This ingredient was already added to the list');
+    } else {
+      this.ingredients.push(ingredient);
+    }
+  }
+
   constructor() {}
 }
--- src/app/services/recipe-service/recipe.service.ts
+++ src/app/services/recipe-service/recipe.service.ts
@@ -35,4 +35,97 @@ export class RecipeService implements OnInit {
   }
 
   constructor(private ingredientsService: IngredientsService) {}
+
+  /**
+   * Compares two ingredients
+   *
+   * @param ingredient1
+   * @param ingredient2
+   * @returns true if the two ingredients are equal
+   */
+
+  compareIngredients(
+    ingredient1: Ingredient,
+    ingredient2: Ingredient
+  ): boolean {
+    return (
+      ingredient1.name.localeCompare(ingredient2.name) === 0 &&
+      ingredient1.halal === ingredient2.halal
+    );
+  }
+
+  /**
+   * Compares if the two arrays of ingredients are equal
+   *
+   * @param ingredients1
+   * @param ingredients2
+   * @returns true if two ingredient arrays are equal
+   */
+
+  compareIngredientArrays(
+    ingredients1: Ingredient[],
+    ingredients2: Ingredient[]
+  ): boolean {
+    let selectedIngredientsEquals = false;
+    let selectedIngredientsCount = 0;
+    ingredients1.forEach((i) => {
+      ingredients2.forEach((ingredient) => {
+        if (this.compareIngredients(i, ingredient)) {
+          ++selectedIngredientsCount;
+        }
+      });
+      if (selectedIngredientsCount === ingredients1.length) {
+        selectedIngredientsEquals = true;
+      }
+      selectedIngredientsCount = 0;
+    });
+    return selectedIngredientsEquals;
+  }
+
+  /**
+   * Compares if the two ingredient names are equal
+   *
+   * @param name1
+   * @param name2
+   * @returns true if names are equal
+   */
+
+  compareNames(name1: string, name2: string): boolean {
+    return name1.localeCompare(name2) === 0;
+  }
+
+  /**
+   * Compares if the recipes are equal
+   *
+   * @param recipe1 first recipe
+   * @param recipe2 second recipe
+   * @returns true if recipes are equal
+   */
+
+  compareRecipes(recipe1: Recipe, recipe2: Recipe): boolean {
+    return (
+      this.compareNames(recipe1.name, recipe2.name) &&
+      this.compareIngredientArrays(recipe1.ingredients, recipe2.ingredients)
+    );
+  }
+
+  /**
+   * Adds the recipe to the list of recipes
+   *
+   * @param recipe
+   */
+
+  addRecipe(recipe: Recipe): void {
+    let comparedRecipes = false;
+    this.recipes?.forEach((r) => {
+      if (this.compareRecipes(r, recipe)) {
+        comparedRecipes = true;
+      }
+    });
+    if (comparedRecipes) {
+      alert('The two recipes are equal');
+    } else {
+      this.recipes?.push(recipe);
+    }
+  }
 }
